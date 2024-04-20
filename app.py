5dc6035295ae296a5a703e6b43b24af8ccf47ef4
Devrim Savas <137702797+devrimsavas@users.noreply.github.com>
2024-04-21 00:29:41 +0200

    some othes

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def removefood():
     your_restaurant.remove_food_from_menu(remove_food_name)
     return render_template('index.html')
 if __name__=="__main__":
-    app.run(port=3000,debug=True)
+    app.run(host='0.0.0.0',port=3000,debug=True)
